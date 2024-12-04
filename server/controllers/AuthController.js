ce69e1881087b18ed9541aaa318aa5e7a3dcc301
GustavoGalarza <gutyxld12@gmail,com>
2024-12-04 08:55:46 -0400

    profile image

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -2,6 +2,7 @@
 import { compare } from "bcrypt";
 import User from "../models/UserModel.js";
 import jwt from "jsonwebtoken"
+import { renameSync, unlinkSync } from "fs"
 
 const maxAge = 3 * 24 * 60 * 60 * 1000;
 
@@ -122,3 +123,51 @@ export const updateProfile = async (request, response, next) => {
         return response.status(500).send("Internal Server Error Auhtcontroller getUserInfo      ");
     }
 };
+
+
+export const addProfileImage = async (request, response, next) => {
+    try {
+        if (!request.file) {
+            return response.status(400).send("Archivo Requerido.")
+        }
+
+        const date = Date.now();
+        let fileName = "uploads/profiles/" + date + request.file.originalname;
+        renameSync(request.file.path, fileName)
+
+        const updatedUser = await User.findByIdAndUpdate(request.userId, { image: fileName }, { new: true, runValidators: true })
+
+        return response.status(200).json({
+           image: updatedUser.image,
+            
+        });
+    } catch ({ error }) {
+        console.log({ error });
+        return response.status(500).send("Internal Server Error Auhtcontroller getUserInfo");
+    }
+};
+
+
+export const removeProfileImage = async (request, response, next) => {
+    try {
+        const { userId } = request;
+        const user =await User.findById(userId);
+
+        if(!user){
+            return response.status(404).send("Usuario no encontrado-image");
+
+        }
+        if(user.image){
+            unlinkSync(user.image)
+        }
+        user.image=null;
+        await user.save();
+
+
+        
+        return response.status(200).send("Imagen del perfil removida");
+    } catch ({ error }) {
+        console.log({ error });
+        return response.status(500).send("Internal Server Error Auhtcontroller getUserInfo");
+    }
+};  
